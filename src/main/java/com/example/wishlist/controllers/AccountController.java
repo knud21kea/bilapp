f16abe60c9f2508395e65b29172f4e75dbafa44e
knud21kea <grah0070@stud.kea.dk>
2022-04-05 21:05:10 +0200

    css fixes

--- src/main/java/com/example/wishlist/controllers/AccountController.java
+++ src/main/java/com/example/wishlist/controllers/AccountController.java
@@ -2,7 +2,6 @@ package com.example.wishlist.controllers;
 
 import com.example.wishlist.models.Account;
 import com.example.wishlist.services.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
